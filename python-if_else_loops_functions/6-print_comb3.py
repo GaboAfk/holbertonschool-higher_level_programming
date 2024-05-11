66c3b1f6161b806c578a725c8f58b3d245e42b5b
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-11 15:12:01 -0300

    format added

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(9):
     for j in range(i, 10):
         if i != j:
-            print(i, j, sep="", end="\n" if i == 8 and j == 9 else ", ")
+            print("{}{}".format(i, j), end="\n" if i == 8 and j == 9 else ", ")
