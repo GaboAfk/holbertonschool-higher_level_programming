e3cf8cb3618f2553c14bc7783c2d1ac5e890752b
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-17 17:19:55 -0300

    Romand update

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     res = 0
-    if isinstance(roman_string, str):
+    if isinstance(roman_string, str) and roman_string is not None:
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        for letter in roman_string:
-            if not res or res >= roman_dict[letter]:
-                res += roman_dict[letter]
+        len_str = len(roman_string)
+        for i in range(len_str):
+            if i + 1 == len_str or roman_dict[roman_string[i]] >= roman_dict[roman_string[i + 1]]:
+                res += roman_dict[roman_string[i]]
             else:
-                res = roman_dict[letter] - res
+                res -= roman_dict[roman_string[i]]
     return res
