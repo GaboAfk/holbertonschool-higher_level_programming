d205315d7c6f608c17f70310fd373cb755104929
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-19 17:14:16 -0300

    raises exceptions

---
+++ python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module provides a class to represent a square with basic validation.
+
+Classes:
+    Square: A class to represent a square with a given size.
+"""
+
+
+class Square:
+    """A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square's side.
+    """
+    def __init__(self, size=0):
+        """Initialize a Square instance.
+
+        Args:
+            size (int, optional): The size of the square's side. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
