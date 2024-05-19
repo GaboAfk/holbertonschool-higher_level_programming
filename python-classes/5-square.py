c2edbf96ea3e00ab2182e72a48977e846d1ad154
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-19 17:55:13 -0300

    # prints

---
+++ python-classes/5-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This module provides a class to represent a square with basic validation
+and a method to calculate its area.
+
+Classes:
+    Square: A class to represent a square with a given size.
+"""
+
+
+class Square:
+    """A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square's side.
+    """
+    def __init__(self, size=0):
+        """Initialize a Square instance.
+
+        Args:
+            size (int, optional): The size of the square's side. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square.
+
+        Returns:
+            int: The size of the square's side.
+        """
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square's side.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self._Square__size ** 2
+
+    def my_print(self):
+        """Prints a square of #.
+        """
+        if self._Square__size == 0:
+            print()
+        else:
+            for i in range(self._Square__size):
+                print("#"*self._Square__size)
