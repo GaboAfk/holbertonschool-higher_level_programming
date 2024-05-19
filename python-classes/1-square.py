d4bc082b89a0fcab9fcbe31a94fc9a796e6e61ce
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-19 16:59:32 -0300

    square size

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module provides a class to represent a square.
+
+Classes:
+    Square: A class to represent a square with a given size.
+"""
+
+
+class Square:
+    """A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square's side.
+    """
+
+    def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square's side.
+        """
+        self._Square__size = size
