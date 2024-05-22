2b379b5bde92278a0c4a32d42c874ae12f13f307
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-22 16:44:25 -0300

    text_indentation tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""text_indentation module
+"""
+
+
+def text_indentation(text):
+    """function that prints a text with 2 new
+    lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): text to print
+
+    Raises:
+        TypeError: only str type accepted
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    phrase = ""
+    space = 0
+    for letter in text:
+        if space:
+            space = 0
+            continue
+        phrase += letter
+        if letter == "." or letter == "?" or letter == ":":
+            print(phrase)
+            print()
+            phrase = ""
+            space = 1
+    print(phrase, end="")
