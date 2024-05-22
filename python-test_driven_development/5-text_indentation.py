c58cb7d63d37437a85c6d4d9fc99df59fea19472
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-22 17:13:25 -0300

    strip function added

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,17 +15,13 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    delimiters = (".", "?", ":")
     phrase = ""
-    space = 0
     for letter in text:
-        if space:
-            space = 0
-            if letter == " ":
-                continue
         phrase += letter
-        if letter == "." or letter == "?" or letter == ":":
-            print(phrase)
+        if letter in delimiters:
+            print(phrase.strip())
             print()
             phrase = ""
-            space = 1
-    print(phrase, end="")
+    if phrase:
+        print(phrase.strip(), end="")
