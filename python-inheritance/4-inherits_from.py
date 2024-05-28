715002be9f53a9eac70ea961c50712e9939fbf96
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-28 19:27:40 -0300

    inherits

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""inherit module
+"""
+
+
+def inherits_from(obj, a_class):
+    """function that checks an object is an instance of a class that inherited
+        (directly or indirectly) from the specified class.
+
+    Args:
+        obj (any): object
+        a_class (type): class to match
+
+    Returns:
+        bool: True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class ; otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
