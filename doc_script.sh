2eb06385eec7b7d9d661be3434d9d7f151f44497
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-28 15:59:15 -0300

    doc script

---
+++ doc_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+_autocomplete_files() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -A file -- "$cur") )
+}
+
+complete -F _autocomplete_files -o filenames read
+
+echo 'file.py name?'
+read -e -i "" files_to_add
+
+# Function to check and print docstrings
+check_docstrings() {
+    #local module="$1"
+    python3 -c "
+
+try:
+    print(__import__("$files_to_add").__doc__)
+except AttributeError:
+    print('modules should have a documentation')
+
+try:
+    print(__import__("$files_to_add").MyClass.__doc__)
+except AttributeError:
+    print('classes should have a documentation')
+
+try:
+    print(__import__("$files_to_add").my_function.__doc__)
+except AttributeError:
+    print('functions (inside and outside a class) should have a documentation')
+
+try:
+    print(__import__("$files_to_add").MyClass.my_function.__doc__)
+except AttributeError:
+    print('functions (inside and outside a class) should have a documentation')
+"
+}
+
+# Call the function with the user input
+check_docstrings "$files_to_add"
