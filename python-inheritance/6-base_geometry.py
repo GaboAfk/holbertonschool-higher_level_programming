00677f5074029a1259f77d17f627c9912c9e240b
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-28 19:49:40 -0300

    area exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module
+"""
+
+
+class BaseGeometry():
+    """BaseGeometry class
+    """
+    def area(self):
+        """area
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
