453c990c569f9898dfa2413a514311126e3fc674
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-01 23:52:54 -0300

    PEP8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""module read_file
+"""
+
 
 def read_file(filename=""):
+    """read a file
+
+    Args:
+        filename (str, optional): file to read. Defaults to "".
+    """
     with open(filename, "r") as file:
         print(file.read())
