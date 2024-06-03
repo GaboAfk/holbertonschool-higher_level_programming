155782b2dde8e97d477549dcefcf03af2af116d0
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-03 20:25:53 -0300

    list dict

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""student module
+
+Returns:
+    dict: dict of class student
+"""
+
+
+class Student():
+    """student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dic = self.__dict__
+        return {att: dic[att] for att in attrs if att in dic}
