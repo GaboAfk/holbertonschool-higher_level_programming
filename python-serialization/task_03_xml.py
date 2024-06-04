03023a195cac8a2302ae107803b1d1cba81149b3
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-04 04:00:56 -0300

    xml

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""xml
+
+Returns:
+    file: deserialized xml file
+"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+    for key in dictionary:
+        element = ET.SubElement(root, key)
+        element.text = str(dictionary[key])
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    xml_data = {}
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for element in root:
+        if isinstance(element.text, int):
+            xml_data[element.tag] = int(element.text)
+        try:
+            xml_data[element.tag] = float(element.text)
+        except ValueError:
+            xml_data[element.tag] = element.text
+
+    return xml_data
