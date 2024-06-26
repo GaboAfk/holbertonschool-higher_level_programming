c76366779c61474ef68935976998fde532fd948d
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-26 03:54:50 -0300

    only BINARY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     c = db.cursor()
     c.execute("SELECT * FROM states\
-               WHERE name LIKE BINARY '{}'\
+               WHERE BINARY name = '{}'\
                ORDER BY id".format(state_name))
 
     for state in c.fetchall():
