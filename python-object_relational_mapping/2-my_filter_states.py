fbf621b35769d0d9a4d0b8f721c97d6dabfd75d9
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-26 03:46:10 -0300

    state input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values
+   in the states table of hbtn_0e_0_usa where
+   name matches the argument."""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+    )
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states\
+               WHERE name = '{}'\
+               ORDER BY id".format(state_name))
+
+    for state in c.fetchall():
+        print(state)
+
+    c.close()
+    db.close()
