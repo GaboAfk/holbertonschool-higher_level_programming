305d8072b9893705db4d40e8cddaf881f92b3e0d
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-26 03:13:42 -0300

    doc????

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa
+"""
+lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    """pep8?"""
-
-    username_ = argv[1]
-    password_ = argv[2]
-    database_ = argv[3]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
 db = MySQLdb.connect(
     host="localhost",
     port=3306,
-    user=username_,
-    password=password_,
-    database=database_
+    user=username,
+    password=password,
+    database=database
 )
 
 c = db.cursor()
 c.execute("SELECT * FROM states ORDER BY id")
 
-[print(states) for states in c.fetchall()]
+for state in c.fetchall():
+    print(state)
 
 c.close()
 db.close()
