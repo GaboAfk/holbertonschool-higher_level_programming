c8ae0d70a6f07c576aa5f0016bff902b3e7f268b
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-27 01:38:43 -0300

    state input match

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed
+    as argument from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = session.query(State)\
+        .filter(State.name == state_name).order_by(State.id).first()
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Not found")
+
+    """#python way
+    states = session.query(State).order_by(State.id)
+    match = False
+    for state in states:
+        if state_name == state.name:
+            match = True
+            print(f"{state.id}: {state.name}")
+    if not match:
+        print("Not found")"""
+
+    session.close()
